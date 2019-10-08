35253d9a18246eee4b07dbdd425281082ccf91b2
大板栗 <576839360@qq.com>
2019-10-08 13:55:50 +0800

    📝 Update types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,11 @@
 declare module 'matter-js'
+
+declare namespace PIXI {
+    export interface Enchance {
+        physify(options: any): void
+    }
+    export interface Sprite extends Enchance { }
+    export interface Text extends Enchance { }
+    export interface Container extends Enchance { }
+    export interface Graphics extends Enchance { }
+}
